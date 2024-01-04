ab831b2db15cf27d83cf78e7679e1a20e76aff39
gganj123 <gganj123@gmail.com>
2024-01-04 20:59:12 +0900

    first commit

---
+++ nomad-1230/app.js
@@ -0,0 +1,14 @@
+const loginForm = document.getElementById("login-form");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = loginForm.querySelector("#login-form button");
+
+function onLoginBtnClick(){
+    const value = loginInput.value;
+    if(value ===""){
+        alert("pleasr write your name");
+    }else if (username.length>=15){
+        alert("you name is too long.");
+    }
+}
+
+loginButton.addEventListener("click",onLoginBtnClick);
