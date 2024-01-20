3aede30f6b698bc4687a36a83208edfe612181f7
gganj123 <gganj123@gmail.com>
2024-01-20 23:56:16 +0900

    study: elice0120 , triangle,string,lopp

---
+++ elice -0120/triangle/index.js
@@ -0,0 +1,21 @@
+/*
+3. 기존의 행보다 1만큼 짧아진 행을 생성하고 color()함수로 대체된 문자열을 반환하세요.
+*/
+
+function triangle(row) {
+  
+}
+
+/*
+  1. input String(row)의 조건을 충족하는지 확인하세요.
+  조건: 입력 문자열에는 대문자 ‘B’, ‘G’ 또는 ‘R’만 포함되어야 한다.
+  2. 지시사항의 규칙데로 두 쌍의 색을 비교해서 새로운 색상을 반환하는 코드를 작성합니다.
+  */
+
+function color(c1, c2) {
+  return null;
+}
+
+console.log(triangle("RBRGB"));
+
+module.exports = { triangle };
